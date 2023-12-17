335e33626adf2b82dea67734da8955b2d23718d1
SerhiiSkyba <128896418+SerhiiSkyba@users.noreply.github.com>
2023-12-17 09:57:59 +0100

    Alpha 0.0.2

--- Main.py
+++ Main.py
@@ -1,38 +1,35 @@
-# This is a code to SoundSpot player.
-# Thanks to our programer Igor Kowalski for realization of this project
+# This is the code for SoundSpot player.
+# Thanks to our programmer Igor Kowalski for realization of this project
 
 # Importing main extensions
 from tkinter import *
 import customtkinter as ctk
 import glob,os,pygame,datetime
-from mutagen.mp3 import MP3 # For playing MP3 files
+from mutagen.mp3 import MP3
 
-# For controling volume
+# For controlling volume
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
-window = ctk.CTk(fg_color="#302D2D",)
+window = ctk.CTk(fg_color="#211E1E",)
 window.geometry("1080x720")
 window.title("SoundSpot (Version Alpha 0.0.1)")
 window.resizable("False","False")
 pygame.mixer.init()
 i = 0
-dlugosc1 = 0
+dlugosc1 = 0  # variable for getting the legth of playing file 
 
-# Stands for recieving all Mp3 files from folder
+# Importing all mp3 files from folder to a list
 files = list(glob.glob('sounds/*.mp3',  
                    recursive = True))
 print(files)
 
 nr_ofFiles = len(files) - 1 
 isplaying = ""
-window.wm_attributes("-transparent","#000000")
-
-print(files)
 
 # Commands
-def Volchange(val): # Stands for changing volume up and down
+def Volchange(val): # Changing volume
 	devices = AudioUtilities.GetSpeakers()
 	interface = devices.Activate(
 	   IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
@@ -40,8 +37,6 @@ def Volchange(val): # Stands for changing volume up and down
 	 
 	# Control volume
 	print(Volvar.get())
-	#volume.SetMasterVolumeLevel(-0.0, None) #max
-	#volume.SetMasterVolumeLevel(-5.0, None) #72%
 	volume.SetMasterVolumeLevel(-50+(Volvar.get()/2), None) #51%
 
 def playing1(): # Change to next song
@@ -95,10 +90,10 @@ def playing2(): # Change to previous song
     except:
         i = len(files)-1
 
-def sound(): # Stands for song state
+def sound():
     global i,PlayButtonImage, filenr, NowPlaying,isplaying, TimelineLength, dlugosc1
     Volchange
-    if (i==0): # In that case, music would be in playing state
+    if (i==0): # In this case, music is played
         pygame.mixer.music.load(files[filenr])
         pygame.mixer.music.play(loops=0)
 
@@ -114,12 +109,10 @@ def sound(): # Stands for song state
         audio = MP3(files[filenr])
         dlugosc1 = audio.info.length
         TimelineLength = dlugosc1/10
-        #Draw()
-        #Refresh()
         dlugosc2 = str(datetime.timedelta(seconds=dlugosc1))
         print(dlugosc2[0:7])
                 
-    elif (i%2==1): # In that case, music would be in pause state
+    elif (i%2==1): # In this case, music is paused
         pygame.mixer.music.pause()
         print ("paused sound")
         print("now playing: ", files[filenr])
@@ -128,8 +121,6 @@ def sound(): # Stands for song state
         audio = MP3(files[filenr])
         dlugosc1 = audio.info.length
         TimelineLength = dlugosc1/10
-        #Draw()
-        #Refresh()
         dlugosc2 = str(datetime.timedelta(seconds=dlugosc1))
         print(dlugosc2[0:7])
 
@@ -145,13 +136,11 @@ def sound(): # Stands for song state
         audio = MP3(files[filenr])
         dlugosc1 = audio.info.length
         TimelineLength = dlugosc1/10
-        #Draw()
-        #Refresh()
         dlugosc2 = str(datetime.timedelta(seconds=dlugosc1))
         print(dlugosc2[0:7])
     i = i+1
 
-def przelacz(x): # Stands for music buttons
+def przelacz(x): # Playlist functionality
     global i,PlayButtonImage, filenr, NowPlaying,isplaying,songstate, TimelineLength, dlugosc1
     Volchange
     pygame.mixer.music.load(files[x])
@@ -168,15 +157,11 @@ def przelacz(x): # Stands for music buttons
     audio = MP3(files[x])
     dlugosc1 = audio.info.length
     TimelineLength = dlugosc1/10
-    #Draw()
-    #Refresh()
     dlugosc2 = str(datetime.timedelta(seconds=dlugosc1))
     print(dlugosc2[0:7])
     i=i+1
 
-
-
-def skip(): # Stands for changing music to next one
+def skip(): # Function for skipping to another file
     global filenr, nr_ofFiles, songstate
     if (filenr <= nr_ofFiles):
         filenr = filenr + 1
@@ -184,27 +169,59 @@ def skip(): # Stands for changing music to next one
     else:
         filenr = 0
 
-def goback(): # Stands for changing music to previous one
+def goback(): # Function for going back to the previous file
     global filenr, songstate
     if (filenr != 0):
         filenr = filenr - 1
         songstate = -1
 
-def Draw():
-    global progressbar, Timelinestate, TimelineLength
-    progressbar.step(Timelinestate)
-
-def Refresh():
-    global progressbar, Timelinestate, TimelineLength, dlugosc1
-    if (Timelinestate < TimelineLength): 
-        Timelinestate = Timelinestate + (dlugosc1 * 0.0001)
-        progressbar.config(variable = TimelineLength)
-        progressbar.step(Timelinestate)
-        window.after(1000,Refresh)
-
-
-
-
+def ThemeChange():
+    global BgMode
+    if (BgMode == "dark"):
+        BgMode = "light"
+        BG.config(file = "textures\\bg_L.png")
+        ArrowSkip.config(file = "textures\\Next_L.png")
+        ArrowBack.config(file = "textures\\Previous_L.png")
+        PlayButtonImage.config(file = "textures\\Play_L.png")
+        HomeImage.config(file = "textures\\HOME_L.png")
+        VolumeImage.config(file = "textures\\Volume_L.png")
+        LogoImage.config(file = "textures\\SoundSpot_L.png")
+        LabelImage.config(file = "textures\\Label_L.png")
+        LineImage.config(file = "textures\\Line_L.png")
+        window.configure(fg_color="#DEE1E1")
+        NowPlayingLabel.configure(fg_color="#FFFFFF",text_color="#000000")
+        GUIFrame.configure(fg_color="#DEE1E1")
+        PlaylistFrame.configure(fg_color="#CFD2D2")
+        LineLabel.configure(fg_color="#DEE1E1")
+        LabelLabel.configure(fg_color="#DEE1E1")
+        HomeLabel.configure(fg_color="#DEE1E1")
+        LogoLabel.configure(fg_color="#DEE1E1")
+        VolumeIcon.configure(fg_color="#DEE1E1")
+        btn.configure(fg_color="#FFFFFF", text_color="#000000")
+        Volume.configure(button_color="#000000")
+    elif (BgMode == "light"):
+        BgMode = "dark"
+        BG.config(file = "textures\\bg.png")
+        ArrowSkip.config(file = "textures\\Next.png")
+        ArrowBack.config(file = "textures\\Previous.png")
+        PlayButtonImage.config(file = "textures\\Play.png")
+        HomeImage.config(file = "textures\\HOME.png")
+        VolumeImage.config(file = "textures\\Volume.png")
+        LogoImage.config(file = "textures\\SoundSpot.png")
+        LabelImage.config(file = "textures\\Label.png")
+        LineImage.config(file = "textures\\Line.png")
+        window.configure(fg_color="#211E1E")
+        NowPlayingLabel.configure(fg_color="#302D2D",text_color="#FFFFFF")
+        GUIFrame.configure(fg_color="#211E1E")
+        PlaylistFrame.configure(fg_color="#302D2D")
+        LineLabel.configure(fg_color="#211E1E")
+        LabelLabel.configure(fg_color="#211E1E")
+        HomeLabel.configure(fg_color="#211E1E")
+        LogoLabel.configure(fg_color="#211E1E")
+        VolumeIcon.configure(fg_color="#211E1E")
+        btn.configure(fg_color="#686464",text_color="#FFFFFF")
+        Volume.configure(button_color = "#D9D9D9")
+        
 
 # Creating GUI widgets
 Image1 = PhotoImage(file = "textures\\image 1.png")
@@ -224,7 +241,9 @@ LineImage = PhotoImage(file = "textures\\Line.png")
 ListImage = [Image1,Image2,Image3]
 
 # Creating GUI frames
-BG = PhotoImage(file = "textures\\bg.png") 
+BG = PhotoImage(file = "textures\\bg.png")
+BgMode = "dark"
+
 BGLabel = ctk.CTkLabel(window,
     image = BG,
     text = "")
@@ -239,8 +258,8 @@ PlaylistFrame = ctk.CTkFrame(window,
 	bg_color = "transparent",
 	)
 
-NowPlaying = "a"
-NowPlaying2 = StringVar()
+NowPlaying = "a"  # Name of the playing file used for functions
+NowPlaying2 = StringVar()  # Name of the playing file shown in the app
 
 NowPlayingLabel = ctk.CTkButton(window,
 	image = Image4,
@@ -252,8 +271,6 @@ NowPlayingLabel = ctk.CTkButton(window,
 	state = "DISABLED",
 	corner_radius = 16
 	)
-
-VolumeUp = ctk.CTkButton(GUIFrame,command=lambda:c)
 
 for p in range(0,nr_ofFiles+1): # Generating music buttons
     btn = ctk.CTkButton(PlaylistFrame,
@@ -269,9 +286,6 @@ for p in range(0,nr_ofFiles+1): # Generating music buttons
     	height = 36,
     	command = lambda p = p:przelacz(p))
     btn.pack(ipadx = 21,pady = 4)
-
-
-pixel = PhotoImage(height = 1,width = 1)
 
 PlaceHolder = ctk.CTkFrame(window,
 	bg_color = "transparent",
@@ -318,7 +332,6 @@ VolumeIcon = ctk.CTkLabel(window,
 	height = 14,
 	)
 
-
 BtnSkip = ctk.CTkButton(GUIFrame,
 	image = ArrowSkip,
 	text = "",
@@ -337,31 +350,34 @@ BtnGoBack = ctk.CTkButton(GUIFrame,
 	width = 50,
 	command = lambda:playing2())
 
-
 Volvar = DoubleVar() # Stands for volume
 
 Volume = ctk.CTkSlider(window,
 	variable = Volvar,
 	from_ = 0,
-	to = 100,
+	to = 100,   
 	bg_color = "#211E1E",
 	button_color = "#D9D9D9",
 	progress_color = "#10B93F",
 	command = Volchange
 	)
 
+BtnBgMode = ctk.CTkButton(window,
+    text= "Change Theme",
+    height = 60,
+    width = 30,
+    command = ThemeChange
+    )
+
 # Creating starting values
-filenr = 1 
-songstate = 0 # Stands for song state
-NowPlaying = files[filenr] # What is music now playing
+filenr = 0 # Number of the playing file in folder
+songstate = 0  #State of the song, 0 = playing, 1 = skip, -1 = go back
+NowPlaying = files[filenr] # What music is currently playing
 NowPlaying = NowPlaying.replace("sounds\\","")
 NowPlaying2.set("Now Playing: " + NowPlaying)
 
 Timelinestate = 0
 TimelineLength = 0
-
-
-
 
 # Creating wigets
 HomeLabel.place(x = 77, y = 69)
@@ -378,10 +394,6 @@ BtnPlay.pack(side = LEFT)
 BtnSkip.pack(side = LEFT)
 VolumeIcon.place(x = 855, y = 626)
 Volume.place(x = 884, y = 626)
-
-
-#Draw()
-#Refresh()
-
+BtnBgMode.place(x = 10,y = 650)
 
 window.mainloop()
